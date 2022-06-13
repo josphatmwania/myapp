7bc6d09970c7e6746f5d33a7db2d4c630770179c
j23747 <josphatmwania237@gmail.com>
2022-06-13 14:24:33 +0300

    Add Button click listerner

--- app/src/main/java/com/myprojects/myapplication/MainActivity.kt
+++ app/src/main/java/com/myprojects/myapplication/MainActivity.kt
@@ -2,10 +2,20 @@ package com.myprojects.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+// declare unchangeable variable of type button and assign it to findViewById
+
+        val previewButton: Button = findViewById(R.id.button_preview)
+        previewButton.setOnClickListener {
+
+        }
+
+
+
     }
 }
