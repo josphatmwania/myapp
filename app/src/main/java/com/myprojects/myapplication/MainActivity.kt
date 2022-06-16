aafa13b1cdd16319ca19de8f38d7f856bd50b149
j23747 <josphatmwania237@gmail.com>
2022-06-16 10:25:01 +0300

    Added a Toast from Text input

--- app/src/main/java/com/myprojects/myapplication/MainActivity.kt
+++ app/src/main/java/com/myprojects/myapplication/MainActivity.kt
@@ -1,22 +1,38 @@
 package com.myprojects.myapplication
+//package arb.test.text.input.layout
+
+
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.Toast
+import com.google.android.material.textfield.TextInputEditText
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-// declare unchangeable variable of type button and assign it to findViewById
+// declare fixed values variable of type button and assign it to findViewById
 
         val previewButton: Button = findViewById(R.id.button_preview)
         previewButton.setOnClickListener {
 
-            Toast.makeText(this, "My button works!", Toast.LENGTH_SHORT).show()
+//            Toast.makeText(this, "My button works!", Toast.LENGTH_SHORT).show()
+
+            onPreviewClicked()
+
 
         }
+
+    }
+
+    private fun onPreviewClicked() {
+        val contactEditText = findViewById<TextInputEditText >(R.id.contact_name)
+        val contactNumberText = findViewById<TextInputEditText>(R.id.contact_number)
+        val testString = contactEditText.text.toString() + ", " + contactNumberText.text.toString()
+        Toast.makeText(this, testString, Toast.LENGTH_SHORT).show()
 
 
 
