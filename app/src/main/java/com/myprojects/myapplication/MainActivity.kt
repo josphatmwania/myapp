276f600c61ce974292befc05a8c9f3f7ab2f8fa8
j23747 <josphatmwania237@gmail.com>
2022-08-05 20:30:46 +0300

    Add button onclick methods & Constants

--- app/src/main/java/com/myprojects/myapplication/MainActivity.kt
+++ app/src/main/java/com/myprojects/myapplication/MainActivity.kt
@@ -5,6 +5,7 @@ package com.myprojects.myapplication
 //import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Toast
 import com.myprojects.myapplication.databinding.ActivityMainBinding
 
@@ -21,10 +22,26 @@ class MainActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
-//        setSupportActionBar(toolbar)
-
         }
 
+//    Save app username in sharePreferences method
+
+    fun saveName(view: View) {
+
     }
+
+//    Search repositories on github method after passing data to DisplayActivity
+
+    fun listRepositories(view: View) {
+//        etRepo
+
+    }
+
+//    Search repos of a particular github user method after passing data to DisplayActivity
+    fun listUserRepositories(view: View) {
+
+    }
+
+}
 
 
